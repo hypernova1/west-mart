d2e6d64b7dff7b723b7f6fadfeac51002b3aedc2
hypernova1 <chtlstjd01@gmail.com>
2021-04-29 22:02:19 +0900

    docs 설정 값 분리

--- config/config.ts
+++ config/config.ts
@@ -18,24 +18,24 @@ interface IConfigGroup {
 
 const config: IConfigGroup = {
   development: {
-    username: "root",
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "west_mart",
-    host: "127.0.0.1",
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   test: {
-    username: "root",
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "west_mart",
-    host: "127.0.0.1",
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql"
   },
   production: {
-    username: "root",
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "west_mart",
-    host: "127.0.0.1",
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql"
   }
 }
