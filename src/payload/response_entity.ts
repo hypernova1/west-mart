d79be9882b16a81b4e5be9603963002e9c923825
hypernova1 <hypemova@gmail.com>
2021-05-01 19:52:37 +0900

    feat: ResponseEntity 정적 메서드 추가

--- src/payload/response_entity.ts
+++ src/payload/response_entity.ts
@@ -2,9 +2,30 @@ import HttpStatus from '@constant/http_status';
 
 export default class ResponseEntity {
     status: HttpStatus;
-    message: string;
-
-    static create(status: HttpStatus, message: string): ResponseEntity {
-        return { status, message };
+    static create(status: HttpStatus, body: object): ResponseEntity {
+        return { status, body } as ResponseEntity;
     }
+
+    body: object;
+
+    static ok(body: object): ResponseEntity {
+        return { status: HttpStatus.OK, body } as ResponseEntity;
+    }
+
+    static created(body: object): ResponseEntity {
+        return { status: HttpStatus.CREATED, body }
+    }
+
+    static noContent(): ResponseEntity {
+        return { status: HttpStatus.NO_CONTENT, body: null } as ResponseEntity;
+    }
+
+    static badRequest(body: object) {
+        return { status: HttpStatus.BAD_REQUEST, body } as ResponseEntity;
+    }
+
+    static forbidden(body: object) {
+        return { status: HttpStatus.FORBIDDEN, body } as ResponseEntity;
+    }
+
 }
--- src/util/error_handler.ts
+++ src/util/error_handler.ts
@@ -2,5 +2,5 @@ import { Response } from 'express';
 import ResponseEntity from '@payload/response_entity';
 
 export default function errorHandler(res: Response, responseEntity: ResponseEntity) {
-    return res.status(responseEntity.status).json({ message: responseEntity.message })
+    return res.status(responseEntity.status).json({ message: responseEntity.body })
 }
