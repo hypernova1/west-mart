747e4ad05887cfdf6056336da81c8af01363a9c5
hypernova1 <chtlstjd01@gmail.com>
2021-05-13 14:57:05 +0900

    test: mocking 테스트

--- test/service/post.ts
+++ test/service/post.ts
@@ -1,9 +1,9 @@
 import 'tsconfig-paths/register';
 import 'mocha';
 import * as chai from 'chai';
+import chaiAsPromised = require('chai-as-promised');
 import * as sinon from 'sinon';
 import PostService from '../../src/service/post_service';
-import { PostDetail, PostListDto, PostListRequest } from '../../src/payload/post';
 import PostRepository from '../../src/repository/post_repository';
 import FavoritePostRepository from '../../src/repository/favorite_post_repository';
 import CategoryRepository from '../../src/repository/category_repository';
@@ -11,6 +11,9 @@ import TagService from '../../src/service/tag_service';
 import TagRepository from '../../src/repository/tag_repository';
 import NotFoundError from '../../src/error/not_found_error';
 import Post from '../../src/model/post';
+import User from '../../src/model/user';
+
+chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
@@ -22,6 +25,35 @@ describe('test getPostDetail', () => {
 
     const postService = new PostService(postRepository, favoritePostRepository, categoryRepository, tagService);
 
+    let sinonStub: sinon.SinonStub<[id: number], Promise<Post>>
+    beforeEach(() => {
+        sinonStub = sinon.stub(postRepository, 'findById');
+    })
+    afterEach(() => {
+        sinonStub.restore();
+    });
+
+    it('found post', async () => {
+        sinonStub.returns(Promise.resolve({
+            id: 1,
+            title: 'title',
+            content: 'content',
+            tags: [],
+            writer: {
+                id: 1,
+                nickname: 'sam',
+            } as User
+        } as Post));
+        const post = await postService.getPostDetail(1);
+        console.log(post);
+        await expect(post).to.be.ok;
+    });
+
+
+    it('not found post', async () => {
+        sinonStub.returns(Promise.resolve(null));
+        await expect(postService.getPostDetail(1)).to.rejectedWith(NotFoundError);
+    });
 });
 
 describe('test', () => {
@@ -31,11 +63,12 @@ describe('test', () => {
     const tagService = new TagService(new TagRepository());
 
     const postService = new PostService(postRepository, favoritePostRepository, categoryRepository, tagService);
-    const findById = sinon.spy(PostRepository.prototype, 'findById');
+
+    const findById = sinon.spy(postRepository, 'findById');
 
     it('test', () => {
         postService.getPostDetail(1);
         sinon.assert.calledWith(findById, 1);
         expect(findById.callCount).to.be.eq(1);
-    })
-})
+    });
+});
