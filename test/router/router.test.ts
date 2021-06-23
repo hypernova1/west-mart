801aa439d16c2e768cf85feeeca5ac4f15d1bb7c
hypernova1 <chtlstjd01@gmail.com>
2021-06-23 19:42:01 +0900

    test: 테스트 코드 추가

--- test/router/router.test.ts
+++ test/router/router.test.ts
@@ -1,23 +1,32 @@
 import 'module-alias/register';
 import 'mocha';
-import chai from 'chai';
+import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import * as sinon from 'sinon';
-import app from '../../src/application';
-import {describe} from 'mocha';
+import Application from '../../src/application';
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
 
-describe('test', () => {
-    return chai.request(app)
-        .post('/auth')
-        .field({
-            email: 'test title',
-            password: 'test desc',
-        })
-        .then((res) => {
-            return expect(res.status).to.eq(200);
-        });
+const app = new Application();
+app.setRouter();
+app.setSequelize();
+app.setMiddleware();
+
+describe('router test', () => {
+    it('login test', () => {
+        return chai.request(app.application)
+            .post('/auth/login')
+            .set('content-type', 'application/json')
+            .send({
+                email: 'test title',
+                password: 'test desc',
+            })
+            .then((res) => {
+                console.log(res);
+                return expect(res.status).to.eq(422);
+            })
+    })
+
 })
--- test/service/post.ts
+++ test/service/post.ts
@@ -1,18 +1,6 @@
 import 'tsconfig-paths/register';
 import 'mocha';
 import * as chai from 'chai';
-import chaiAsPromised = require('chai-as-promised');
-import * as sinon from 'sinon';
-
-import PostService from '../../src/service/post_service';
-import TagService from '../../src/service/tag_service';
-import NotFoundError from '../../src/error/not_found_error';
-import Post from '../../src/model/post';
-import User from '../../src/model/user';
-
-chai.use(chaiAsPromised);
-
-const expect = chai.expect;
 
 describe('test', () => {
 
