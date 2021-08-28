3a5d9ad2a43d8b948b0d30bbcd8265594d1ecf78
hypenova1 <hypemova@gmail.com>
2021-08-28 12:12:42 +0900

    feat: 유저 승인시 카테고리 생성, 유저 삭제시 카테고리 삭제 로직 추가

--- src/service/admin_service.ts
+++ src/service/admin_service.ts
@@ -3,11 +3,14 @@ import { Service } from 'typedi';
 import { Repository } from 'sequelize-typescript';
 import sequelize from '@model/index';
 import User from '@model/user';
+import Category from '@model/category';
 
 @Service()
 export default class AdminService {
-  constructor(private userRepository: Repository<User>) {
+  constructor(private userRepository: Repository<User>,
+              private categoryRepository: Repository<Category>) {
     this.userRepository = sequelize.getRepository(User);
+    this.categoryRepository = sequelize.getRepository(Category);
   }
 
   async approveUser(userId: number): Promise<void> {
@@ -17,6 +20,20 @@ export default class AdminService {
     if (user.isApprove) {
       throw new BadRequestError('이미 승인된 사용자입니다.');
     }
+
+    const categoryCount = await this.categoryRepository.count({
+      where: {
+        isActive: true,
+      }
+    });
+
+    console.log(user.id);
+
+    await this.categoryRepository.create({
+      name: user.nickname,
+      sequence: categoryCount + 1,
+      userId: user.id,
+    });
 
     await user.update({ isApprove: true });
   }
@@ -28,6 +45,14 @@ export default class AdminService {
     if (!user.isApprove) {
       throw new BadRequestError('승인 처리 되지 않은 사용자입니다.');
     }
+
+    await this.categoryRepository.update({
+      isActive: false,
+    }, {
+      where: {
+        userId: user.id,
+      }
+    })
 
     await user.update({ isApprove: false });
   }
